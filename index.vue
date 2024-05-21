318cbd0d509e67d0f64ea74acb2d9785e436b793
Pascal Aigner <pascal.aigner97@outlook.com>
2024-05-21 23:40:21 +0200

    first commit

---
+++ index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="title">Hello World</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      this.setAnimation()
+    },
+
+    methods: {
+      setAnimation() {
+        this.$anime({
+          targets: '.title',
+          translateX: 250,
+          rotate: '1turn',
+          backgroundColor: '#FFF',
+          duration: 800
+        })
+      }
+    }
+  }
+</script>
